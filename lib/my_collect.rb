05f055b781011bde52bce65c6931323601c586bc
jtang93 <tang.jonathan93@gmail.com>
2018-09-13 07:12:54 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,9 @@
 def my_collect(array)
   i = 0
+  newarr = []
   while i < array.length
-    yield(array[i])
+    newarr << yield(array[i])
     i+=1
   end
-  
+  newarr
 end
-
-array = []
-my_collect(array) do |item|
-  
